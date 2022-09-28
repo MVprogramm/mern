92b5331dfcb0bb31bf740cd31d8e2a84f56abbb4
valiahmetov <valiahmetov@ukr.net>
2022-09-28 23:30:27 +0300

    add frontend

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const config = require("config")
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = Router();
@@ -39,6 +41,39 @@ router.post(
 );
 
 // /api/auth/login
-router.post("/login", async (req, res) => {});
+router.post(
+  "/login",
+  [
+    check("email", "Invalid email address").normalizeEmail().isEmail(),
+    check("password", "Missing the password").exists(),
+  ],
+  async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) return res.status(400).json({
+            errors: errors.array(),
+            message: 'Invalid registration data'
+        })
+
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user)
+        return res
+          .status(400)
+          .json({ message: "This email address doesn't exist" });
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({
+        message: "Invalid password"
+      })
+      const token = jwt.sigh(
+        { userId: user.id },
+        config.get("jwtSecret"),
+        { expiresIn: "1h" }
+      );
+      return res.json({ token, userId: user.id });
+    } catch (error) {
+      res.status(500).json({ message: `Server Error: ${error.message}` });
+    }
+});
 
 module.exports = router;
